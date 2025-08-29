625e3ff7ead0cc6482d23c13f1ed167f47a48c07
StirLinX <bolusovnursultan@gmail.com>
2025-08-29 17:23:03 +0500

    Update main.js

--- main.js
+++ main.js
@@ -15,35 +15,27 @@ alert(result)
 function RandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
 function RandomOperator() {
   const operators = ['+', '-', '*', '/'];
   return operators[Math.floor(Math.random() * operators.length)];
 }
-
 function generator(count) {
-  for (let i = 0; i < count; i++) {
+    
+  for(let i = 0; i < count; i++) {
     const num1 = RandomInt(1, 20);
     const num2 = RandomInt(1, 20);
     const operator = RandomOperator();
-
-    // Генерируем пример как строку
     const example = `${num1} ${operator} ${num2}`;
-    // Вычисляем правильный ответ
     const correctAnswer = eval(example);
-
-    // Спрашиваем ответ у пользователя
     const userAnswer = +prompt(`Решите пример: ${example}`);
 
-    // Проверяем правильность
-    if (userAnswer === correctAnswer) {
+    if(userAnswer === correctAnswer) {
       alert(`Ваш ответ верный: ${userAnswer}`);
-    } else {
+    }else {
       alert(`Ваш ответ не верный:  ${userAnswer}. Правильный ответ:  ${correctAnswer}`);
     }
   }
 }
 
-// Запрос у пользователя количества примеров
 const count = +prompt("Сколько примеров вы хотите решить?");
 generator(count);
